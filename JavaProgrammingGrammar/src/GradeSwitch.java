bcfd214f5ab19bcd13121aee8206c2060abb0c7c
송가영 <sgyoung96@naver.com>
2021-11-05 23:29:22 +0900

    GradeSwitch commit

---
+++ JavaProgrammingGrammar/src/GradeSwitch.java
@@ -0,0 +1,39 @@
+public class GradeSwitch {
+    /*
+    학점이 A, B 인 학생에게는 "참 잘하였습니다.",
+    학점이 C, D 인 학생에게는 "좀 더 노력하세요.",
+    학점이 F 인 학생에게는 "다음 학기에 다시 수강하세요."
+    를 출력하는 프로그램을 switch 문의 break 를 잘 활용하여 작성하라.
+    예외 처리는 "잘못된 학점입니다."
+     */
+    public static void main(String[] args) {
+        String message = printMessage(30);
+        System.out.println(message);
+    }
+
+    private static String printMessage(int score) {
+        String message;
+        char grade;
+
+        if (score > 90) {
+            grade = 'A';
+        } else if (score > 80) {
+            grade = 'B';
+        } else if (score > 70) {
+            grade = 'C';
+        } else if (score > 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        message = switch (grade) {
+            case 'A', 'B' -> "참 잘하였습니다.";
+            case 'C', 'D' -> "좀 더 노력하세요.";
+            case 'F' -> "다음 학기에 다시 수강하세요.";
+            default -> "잘못된 학점입니다.";
+        };
+
+        return message;
+    }
+}
