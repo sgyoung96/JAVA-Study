a3dbeeb8361a0e0a7f524cf4f961bb223feefe43
송가영 <sgyoung96@naver.com>
2021-11-05 23:43:58 +0900

    ForSample 완성

--- JavaProgrammingGrammar/src/ForSample.java
+++ JavaProgrammingGrammar/src/ForSample.java
@@ -1,2 +1,18 @@
+// 예제 3-1
+// for 문을 이용하여 1부터 10까지 덧셈을 표시하고 합을 구하라.
 public class ForSample {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            if (i < 10) {
+                System.out.print(i + " + ");
+            }
+            if (i == 10) {
+                System.out.print(i);
+            }
+            sum = sum + i;
+        }
+        System.out.print(" = " + sum);
+    }
 }
