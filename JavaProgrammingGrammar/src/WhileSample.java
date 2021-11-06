39a1a5756cb6c462210e65e43da0f1251676f81a
송가영 <sgyoung96@naver.com>
2021-11-07 01:30:52 +0900

    03-02 WhileSample inital commit + add comment

---
+++ JavaProgrammingGrammar/src/WhileSample.java
@@ -0,0 +1,10 @@
+// 예제 3-2
+/*
+ while 문을 이용하여 키보드에서 숫자를 여러 개 입력받아 입력받은 수의 평균을 출력하는 프로그램을 작성해보자.
+ 0이 입력되면 반복을 종료하고 평균을 구하여 출력한다.
+ */
+public class WhileSample {
+    public static void main(String[] args) {
+        // 안드로이드 스튜디오로 이동 --> 샘플 앱 제작 (WhileSmaple)
+    }
+}
