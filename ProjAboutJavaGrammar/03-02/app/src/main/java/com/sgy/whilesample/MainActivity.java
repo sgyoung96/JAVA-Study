51565524eb22cd27872073b1b84ad8fc456fa8d8
송가영 <sgyoung96@naver.com>
2021-11-07 12:56:03 +0900

    평균구하기 앱 완성

--- ProjAboutJavaGrammar/03-02/app/src/main/java/com/sgy/whilesample/MainActivity.java
+++ ProjAboutJavaGrammar/03-02/app/src/main/java/com/sgy/whilesample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sgy.whilesample;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,10 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sgy.whilesample.databinding.ActivityMainBinding;
 
 import java.lang.reflect.Array;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 // 예제 3-2
@@ -29,8 +32,12 @@ public class MainActivity extends AppCompatActivity {
     private Button btnCalSum;
     private TextView tvResultSum;
 
-    private String userInput;
     private ArrayList<String> userInputGroup;
+
+    private BigDecimal numSum;
+    private ArrayList<BigDecimal> numSumArr;
+    private BigDecimal numSize;
+    private BigDecimal numAvg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,23 +51,55 @@ public class MainActivity extends AppCompatActivity {
         btnCalSum = binding.btnCalSum;
         tvResultSum = binding.tvResultSum;
 
-        userInput = "";
         userInputGroup = new ArrayList<>();
+
+        numSum = new BigDecimal(0);
+        numSize = new BigDecimal(0);
+        numSumArr = new ArrayList<BigDecimal>();
 
         btnUserInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                userInput = etUserInput.getText().toString();
-                userInputGroup.add(userInput); // 배열에 들어가기는 한다. [1, 1, 1, 1]
-                // TODO 두번 눌러야 출력됨
-                // TODO 1 5 10 3 55 이런 형태로 출력시키고 싶음
-                String numGroup = userInputGroup.toString();
-                String[] num = numGroup.split("");
-                numGroup = num.toString();
-                numGroup.replace("[", "");
-                numGroup.replace("]", "");
-                numGroup.replace(",", "");
+                if (etUserInput.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "값을 입력하세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (etUserInput.getText().toString().equals("0")) {
+                    etUserInput.setText("");
+                    calAvg();
+                    return;
+                }
+
+                userInputGroup.add(etUserInput.getText().toString()); // 배열에 모든 숫자 값 저장(String 형)
+                tvNumGroup.setText(userInputGroup.toString());
+
+                numSumArr.add(new BigDecimal(etUserInput.getText().toString()));
+                for (int i = 0; i < numSumArr.size(); i++) {
+                    numSum = numSum.add(numSumArr.get(i));
+                }
+
+                etUserInput.setText("");
+            }
+
+        });
+
+        btnCalSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etUserInput.getText().toString().equals("0")) {
+                    etUserInput.setText("");
+                }
+                calAvg();
             }
         });
+    }
+
+    private void calAvg() {
+        if (tvNumGroup.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "값을 입력하세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        numSize = new BigDecimal(numSumArr.size());
+        numAvg = new BigDecimal(String.valueOf(numSum.divide(numSize)));
+        tvResultSum.setText(numAvg.toString());
     }
 }
