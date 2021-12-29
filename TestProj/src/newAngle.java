a6caee2a839da341e7680702e3eca5eed0af5ab7
송가영 <sgyoung96@naver.com>
2021-12-29 16:58:07 +0900

    각도 보정값 구하기 완료

--- TestProj/src/newAngle.java
+++ TestProj/src/newAngle.java
@@ -11,12 +11,14 @@ public class newAngle {
     public static double deltaY;     // atan2 에 대입할 y
     public static double angle;      // 각도
 
+    public static double result;     // 보정값
+
     public static void main(String[] args) {
 
         lShoulderX = 0;
         lShoulderY = 0;
-        rShoulderX = 2;
-        rShoulderY = 2;
+        rShoulderX = 0;
+        rShoulderY = 0;
 
         calcAngle(lShoulderX, lShoulderY, rShoulderX, rShoulderY);
     }
@@ -30,7 +32,30 @@ public class newAngle {
         angle = Math.toDegrees(Math.atan2(deltaY, deltaX));
 
         System.out.println(angle);
-        System.out.println(Math.abs(360 - angle));
+        // 절댓값이 180보다 클 때 ---> 절대 안 탐 (max = 180, min = -180)
+        if (angle <= 180 && angle > 0) {             // 양수
+            if (angle == 180) {
+                result = angle;
+                System.out.println(result);
+            } else {
+                result = 180 - angle;
+                System.out.println(result);
+            }
+        } else if (angle >= -180 && angle < 0) {     // 음수
+            if (angle >= -90) {
+                result = 360 - Math.abs(angle);
+                System.out.println(result);
+            } else {
+                result = 360 - angle;
+                System.out.println(result);
+            }
+        } else if (angle == 0) {
+            result = 360;
+            System.out.println(result);
+        } else {
+            result = 0;
+            System.out.println(result);
+        }
 
         /*
             결과 값이 45 / 315 가 나왔으므로, lShoulder 가 기준이 맞고, lShoulder 가 화면상 오른쪽에 위치한, 인체로써는 왼쪽 어깨가 맞음
